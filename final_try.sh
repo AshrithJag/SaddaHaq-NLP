2539b0fe94dcd13c61f390d013d3c99bf65418ef
Ashrith Jalagam <jvashrith@gmail.com>
2015-06-22 13:19:52 +0530

    Deleting

--- final_try.sh
+++
@@ -1,2 +0,0 @@
-python past_new.py $1 > tweets_from_trends.txt
-python sentiment_tweets_from_trends_1.py 
--- tweets_sentiments_trends.sh
+++
@@ -1,4 +0,0 @@
-#assdasd
-python get_trends.py
-python stream_track.py $1 $2
-python sentiment_tweets_from_trends.py
